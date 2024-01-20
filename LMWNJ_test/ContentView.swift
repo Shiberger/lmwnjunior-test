b419254cd282196373f892a779655280ad22e3e7
Shiberger <hannarong.tonkra@gmail.com>
2024-01-21 03:43:27 +0700

    Initial Commit

---
+++ LMWNJ_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LMWNJ_test
+//
+//  Created by Hannarong Kaewkiriya on 21/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LMWNJ_test/LMWNJ_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMWNJ_testApp.swift
+//  LMWNJ_test
+//
+//  Created by Hannarong Kaewkiriya on 21/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct LMWNJ_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
