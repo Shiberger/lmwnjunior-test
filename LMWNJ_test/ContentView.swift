04fa48e635160703acdfb004c024bfdd26212e25
Shiberger <hannarong.tonkra@gmail.com>
2024-01-21 13:48:22 +0700

    add refreshable pull refresh

--- LMWNJ_test/ContentView.swift
+++ LMWNJ_test/ContentView.swift
@@ -36,7 +36,7 @@ struct ContentView: View {
               }
               Divider()
           }
-          .onAppear {
+          .refreshable {
               viewModel.fetch()
           }
       }
